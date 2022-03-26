3654de9b3e3e08178205144f429cff7edb620293
Guigalaverna <guigalaverna@gmail.com>
2022-03-25 22:33:35 -0300

    feat: add fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,13 @@ export default class MyDocument extends Document {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
