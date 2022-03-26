f810c27541bb458b516cc180db8d660aaf60e840
Guigalaverna <guigalaverna@gmail.com>
2022-03-25 23:22:19 -0300

    feat: show sidebar in all application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import { AppProps } from "next/app";
+import { Sidebar } from "../components/Sidebar";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Sidebar />
+      <Component {...pageProps} />
+    </>
+  );
 }
