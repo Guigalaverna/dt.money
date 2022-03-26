d49e021ac6a0b3f70ad003edd68fee482500ea1c
Guigalaverna <guigalaverna@gmail.com>
2022-03-26 14:59:42 -0300

    feat: add flex to Next.js div

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
 import { createStitches } from "@stitches/react";
 
-export const { styled, css, getCssText, globalCss } = createStitches({
+export const { theme, styled, css, getCssText, globalCss } = createStitches({
   theme: {
     fonts: {
       system: "system-ui",
