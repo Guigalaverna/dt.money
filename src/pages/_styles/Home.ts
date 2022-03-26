8d9cb9f7443b726f1f155f9e63c13181b8a120ca
Guigalaverna <guigalaverna@gmail.com>
2022-03-26 14:59:20 -0300

    feat: create styled components

---
+++ src/pages/_styles/Home.ts
@@ -0,0 +1,34 @@
+import { styled } from "../../styles";
+
+export const Container = styled("main", {
+  height: "100%",
+  width: "100%",
+});
+
+export const Header = styled("header", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  height: "30%",
+  width: "100%",
+  background: "$blue",
+  padding: 56 / 16 + "rem",
+
+  "> div": {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export const Heading = styled("h1", {
+  fontFamily: "$heading",
+  color: "$shape",
+});
+
+export const Cards = styled("section", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  marginBottom: "-6rem",
+});
