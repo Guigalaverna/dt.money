eaef983d09e93655e5872d35edad8f7807266123
Guigalaverna <guigalaverna@gmail.com>
2022-04-03 00:25:27 -0300

    feat: create modal

--- src/pages/_styles/Home.ts
+++ src/pages/_styles/Home.ts
@@ -38,3 +38,68 @@ export const Content = styled("main", {
   height: "100%",
   padding: "3.5rem",
 });
+
+export const ModalHeader = styled("header", {
+  h2: {
+    color: "$titles",
+  },
+  marginTop: "1rem",
+  marginBottom: "1.5rem",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+
+  button: {
+    background: "transparent",
+    border: "none",
+    fontSize: "0",
+    cursor: "pointer",
+    transition: "filter 0.2s",
+
+    "&:hover": {
+      filter: "brightness(0.8)",
+    },
+  },
+});
+
+export const Fieldset = styled("fieldset", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+
+  border: "none",
+
+  "div:nth-child(3)": {
+    display: "flex",
+    flexDirection: "column",
+
+    "label:nth-child(2)": {
+      color: "gray",
+      fontSize: "0.9rem",
+      marginBottom: "0.5rem",
+    },
+  },
+});
+
+export const Input = styled("input", {
+  width: "100%",
+  padding: 20,
+  background: "$background",
+  border: "none",
+  borderRadius: "0.25rem",
+});
+
+export const ModalButton = styled("button", {
+  background: "$green",
+  padding: 20,
+  border: "none",
+  borderRadius: "0.25rem",
+  color: "$shape",
+  fontWeight: "bold",
+  fontSize: "1.05rem",
+  transition: "filter 0.2s",
+
+  "&:hover": {
+    filter: "brightness(0.9)",
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Card } from "../components/Card";
 import { TransactionTable } from "../components/TransactionTable";
-import { styled } from "../styles";
-import { Cards, Container, Header, Heading, Content } from "./_styles/Home";
+import {
+  Cards,
+  Container,
+  Header,
+  Heading,
+  Content,
+  Fieldset,
+  ModalHeader,
+  Input,
+  ModalButton,
+} from "./_styles/Home";
 
+import { RiCloseLine } from "react-icons/ri";
+import Modal from "react-modal";
+import { theme } from "../styles";
+
+Modal.setAppElement("#__next");
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
     <Container data-testid="home">
       <Header>
         <div>
           <Heading>Bem vindo, Guilherme</Heading>
-          <Button type="filled" title="Nova transação" />
+          <Button
+            buttonType="filled"
+            title="Nova transação"
+            onClick={openModal}
+          />
         </div>
 
         <Cards>
@@ -22,6 +51,64 @@ export default function Home() {
       <Content>
         <TransactionTable />
       </Content>
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            paddingLeft: 40,
+            paddingTop: 64,
+            paddingBottom: 64,
+            paddingRight: 48,
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+          },
+        }}
+      >
+        <ModalHeader>
+          <h2>Nova transação</h2>
+          <button onClick={closeModal}>
+            <RiCloseLine size={24} color={theme.colors.red.value} />
+          </button>
+        </ModalHeader>
+        <form>
+          <Fieldset>
+            <div>
+              <label htmlFor="title">Título da Transação</label>
+              <Input id="title" type="text" placeholder="Aluguel da Casa" />
+            </div>
+
+            <div>
+              <label htmlFor="type">Tipo da transação</label>
+              <select name="type" id="type">
+                <option value="default">Selecione uma Opção</option>
+                <option value="income">Entrada</option>
+                <option value="outcome">Saída</option>
+              </select>
+            </div>
+
+            <div>
+              <label htmlFor="amount">Valor da Transação</label>
+              <label htmlFor="amount">Obs: digite apenas o número</label>
+              <Input name="amount" type="number" placeholder="10" />
+            </div>
+
+            <div>
+              <label htmlFor="category">Categoria</label>
+              <Input type="text" placeholder="Casa" />
+            </div>
+
+            <ModalButton type="submit">Cadastrar</ModalButton>
+          </Fieldset>
+        </form>
+      </Modal>
     </Container>
   );
 }
