d28c8237adcf7368cfdd96ca3b5e5fb94e93dfaf
Guigalaverna <guigalaverna@gmail.com>
2022-04-03 04:28:04 -0300

    feat: forwarding ref

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import { useTransactions } from "../../contexts/TransactionsContext";
 import { formatCentsInReal } from "../../utils/formatCentsInReal";
 import { Container, Content, Header } from "./styles";
@@ -5,54 +6,58 @@ import { Container, Content, Header } from "./styles";
 import { RiDeleteBin2Line } from "react-icons/ri";
 import { css, theme } from "../../styles";
 import { formatISOtoLocaleDate } from "../../utils/formatISOtoLocaleDate";
+import { ForwardedRef, forwardRef, useRef } from "react";
 
-export function TransactionTable() {
-  const { transactions, removeTransaction } = useTransactions();
+export const TransactionTable = forwardRef(
+  (props, ref: ForwardedRef<HTMLTableSectionElement>) => {
+    const { transactions, removeTransaction } = useTransactions();
+    const tableRef = ref;
 
-  const outcomeCSS = css({
-    color: "$red",
-  });
-  const incomeCSS = css({
-    color: "$green",
-  });
+    const outcomeCSS = css({
+      color: "$red",
+    });
+    const incomeCSS = css({
+      color: "$green",
+    });
 
-  return (
-    <div style={{ height: 570, overflowY: "auto" }}>
-      <Container>
-        <Header>
-          <th>Título</th>
-          <th>Preço</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </Header>
-        <Content>
-          {transactions.map(transaction => {
-            return (
-              <tr key={transaction.id}>
-                <td>{transaction.title}</td>
-                <td
-                  className={
-                    transaction.type === "income" ? incomeCSS() : outcomeCSS()
-                  }
-                >
-                  {transaction.type === "outcome" && "-"}
-                  {formatCentsInReal(transaction.amount)}
-                </td>
-                <td>{transaction.category}</td>
-                <td>{formatISOtoLocaleDate(transaction.createdAt)}</td>
-                <td>
-                  <button onClick={() => removeTransaction(transaction.id)}>
-                    <RiDeleteBin2Line
-                      size={24}
-                      color={theme.colors.red.value}
-                    />
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </Content>
-      </Container>
-    </div>
-  );
-}
+    return (
+      <div style={{ height: 570, overflowY: "auto" }}>
+        <Container>
+          <Header>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </Header>
+          <Content ref={tableRef}>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
+                  <td
+                    className={
+                      transaction.type === "income" ? incomeCSS() : outcomeCSS()
+                    }
+                  >
+                    {transaction.type === "outcome" && "-"}
+                    {formatCentsInReal(transaction.amount)}
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{formatISOtoLocaleDate(transaction.createdAt)}</td>
+                  <td>
+                    <button onClick={() => removeTransaction(transaction.id)}>
+                      <RiDeleteBin2Line
+                        size={24}
+                        color={theme.colors.red.value}
+                      />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </Content>
+        </Container>
+      </div>
+    );
+  }
+);
