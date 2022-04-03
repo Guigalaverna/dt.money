2a1abc3d0662c609dc3dbb21d71f6fa6cfa04883
Guigalaverna <guigalaverna@gmail.com>
2022-04-03 02:28:58 -0300

    fix: erros when test home page

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -3,7 +3,11 @@ import Home from "../../pages";
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    render(<Home />);
+    render(
+      <div id="__next">
+        <Home />
+      </div>
+    );
     const homeContainer = screen.getByTestId("home");
 
     expect(homeContainer).toBeInTheDocument();
