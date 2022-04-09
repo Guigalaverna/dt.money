bded67ece00abd46470abf2de9ce7d0959fc3293
Guigalaverna <guigalaverna@gmail.com>
2022-04-08 22:19:09 -0300

    feat: using real data

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,47 +1,19 @@
-// @ts-nocheck
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import { Container } from "./styles";
 import { useUser } from "../../contexts/UserContext";
+import { useEffect, useState } from "react";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export function PieChart() {
-  const { transactions } = useUser();
+interface PieChartProps {
+  data: any;
+}
 
-  // create a array with objects with key is the category and value is the all transaction of that category
-  const categories = transactions.map(t => t.category);
-
-  const transactionByCategory = transactions.map(transaction => {
-    if (categories.includes(transaction.category)) {
-      return {
-        category: transaction.category,
-        amount: transaction.amount,
-      };
-    }
-  });
-
-  console.log(categories);
-  console.log(transactionByCategory);
-
-  const data = {
-    labels: ["Red", "Blue", "Yellow"],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-      },
-    ],
-  };
+export function PieChart({ data }: PieChartProps) {
   return (
     <Container>
-      <Pie
-        options={{
-          maintainAspectRatio: 20,
-        }}
-        data={data}
-      />
+      <Pie data={data} />
     </Container>
   );
 }
