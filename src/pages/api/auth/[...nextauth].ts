1bfc7e23f49213f1224758a5df129631727c7e20
Guigalaverna <guigalaverna@gmail.com>
2022-04-10 21:02:54 -0300

    feat: add session callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { setDoc, doc, collection, getDoc } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+import { v4 } from "uuid";
+
 export default NextAuth({
   providers: [
     GoogleProvider({
@@ -10,7 +14,26 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
+      try {
+        const usersCollectionRef = collection(db, "users");
+        await setDoc(doc(usersCollectionRef, String(user.id)), {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatar: user.image,
+          createdAt: new Date().toISOString(),
+        });
+      } catch (error) {
+        console.error(error);
+      }
+
       return true;
+    },
+
+    async session({ user, session }) {
+      session.id = user.id;
+
+      return session;
     },
   },
 });
