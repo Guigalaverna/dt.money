fbd08b3bce5802510034d307c180f15b363210e6
Guigalaverna <guigalaverna@gmail.com>
2022-04-10 21:03:09 -0300

    feat: removing unnecessary imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-import { Sidebar } from "../components/Sidebar";
 import { SessionProvider } from "next-auth/react";
 import { UserProvider } from "../contexts/UserContext";
 
